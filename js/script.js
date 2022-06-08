c90cc89cf4ddab5fd2e5f0d3d5929d299e6fcffa
AkromCik <@akromjonrahimjon712>
2022-06-08 09:37:33 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var num = prompt("Summani kiriting")
+var userNum = document.querySelector(".num")
+
+if (num >= 9666000 ){
+  userNum.textContent = "Oq yo'l"
+}else{
+  userNum.textContent = "Ozgina sabir qiling"
+}
